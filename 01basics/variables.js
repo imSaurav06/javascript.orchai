7b7d71a37365a291241d92138465a0bfcd1d9d11
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2025-03-26 19:19:22 +0000

    variable"s

---
+++ 01basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 29469;
+let accountEmail = "ksau@gmail.com"
+var accountPassword = "28456"
+  accountCity = "Jaipur"
+
+
+  accountCity =" patna"
+   let accountEmail = "@gmail"
+  var accountPassword = "93645"
+  //accountId = 11111;
+
+
+console.log(accountId);
+console.table([accountId,accountPassword, accountCity, accounteEmail,])
