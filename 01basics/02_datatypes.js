9999d387ea209d3fcb5eb879c310b39aee231f2c
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2025-03-27 20:58:36 +0000

    datatype

--- 01basics/02_datatypes.js
+++ 01basics/02_datatypes.js
@@ -11,6 +11,14 @@ let state =null ; //object
 let xyz = undefined //undifined
 let zyx             //undifined
 
+
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof isLoggedIn)
+// console.log(typeof xyz)
+// console.log(typeof zyx)
+// console.log(typeof state)
+
 // number => 2^53 limits
 // bigint=   //for large value (stock_m)
 // string => ""
@@ -19,15 +27,24 @@ let zyx             //undifined
 // undefined => 
 // symbol => unique
 
-console.log(typeof name)
-console.log(typeof age)
-console.log(typeof isLoggedIn)
-console.log(typeof xyz)
-console.log(typeof zyx)
-console.log(typeof state)
+
 
 
 // object
 
 // console.log(typeof undefined); // undefined
 // console.log(typeof null); // object
+
+
+
+
+
+let str1 = "ab"
+let str2 = "cd"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 20) //120
+// console.log(100 + "2")  //1002
+console.log("10" + 2 + 2) //1022
+console.log(1 + 2 + "20") //320
