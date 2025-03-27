210a35f57b7fe06cc3a928e62ea109a709917cd4
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2025-03-27 20:35:46 +0000

    Datatypes

---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,33 @@
+//"use strict"; // treat all JS code as newer version
+
+
+// console.log("Hitesh")
+
+
+let name = "hitesh" //string
+let age = 18  // number
+let isLoggedIn = false //boolean
+let state =null ; //object
+let xyz = undefined //undifined
+let zyx             //undifined
+
+// number => 2^53 limits
+// bigint=   //for large value (stock_m)
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof xyz)
+console.log(typeof zyx)
+console.log(typeof state)
+
+
+// object
+
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
