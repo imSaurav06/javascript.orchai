34b46823f7dcd99de7af44b13cff96ac6d32e014
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2025-03-30 20:41:16 +0000

    arrays

---
+++ 02basics/01arrays.js
@@ -0,0 +1,59 @@
+
+/**arr is a object ? */
+
+let arr = [0,22,2,3,4,5];
+const int_arrr = new Array(2,3,4,5)
+const string_arrr = new Array('kasu', 'kasu')
+const char_arrr = new Array('k','a','s','u')
+
+// console.log(arr);
+// console.log(int_arrr);
+// console.log(string_arrr);
+// console.log(char_arrr);
+
+
+// console.log(typeof arr); //object
+// console.log(typeof int_arrr); //object
+//  console.log(typeof string_arrr); //object
+//  console.log(typeof char_arrr); //object
+
+
+// console.log( typeof arr) //number
+
+// const neww = String(arr[1])
+//  console.log(typeof neww) //string
+
+ arr.push(10)
+ 
+ //arr.push('k','k','k')
+ //arr.push(12)
+ console.log( arr);
+
+// arr.unshift(9); //add from first and shift all element
+// arr.shift();   ///remove from first and shift all element
+// arr.shift();
+// console.log( arr);
+
+// console.log(arr.includes(2)); // available or not -- false
+// console.log(arr.indexOf(22));  //1
+// console.log(arr.at(1));  //22
+
+// console.log(arr.slice(0,2)); //print sliced  element 0 to <2 // [0,22] original value no change
+// console.log(arr);
+// console.log(arr.splice(0,3)); //print slicde  element 0 to <2 // [0,22]original value may change
+// console.log(arr);
+
+// arr.splice(1, 0);
+// // Inserts at index 1
+// console.log(arr)
+
+
+
+// const array1 = [1, 30, 4, 21, 100000];
+// array1.sort();
+// console.log(array1);
+// Expected output: Array [1, 100000, 21, 30, 4]
+
+
+
+
