c632fc91ac0602e8f9bce25131a65729f46aa23e
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2026-06-26 21:19:33 +0000

    .

---
+++ Fundamentals _of_js/01basics/02_datatypes.js
@@ -0,0 +1,51 @@
+//"use strict"; // treat all JS code as newer version
+
+
+// console.log("Hitesh") 
+  
+
+let name = "hitesh" //string
+let age = 18  // number
+let isLoggedIn = false //boolean
+let state =null ; //object
+let xyz = undefined //undifined
+let zyx             //undifined
+
+
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof isLoggedIn)
+// console.log(typeof xyz)
+// console.log(typeof zyx)
+// console.log(typeof state)
+
+// number => 2^53 limits
+// bigint=   //for large value (stock_m)
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+
+// object
+
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+
+
+
+
+let str1 = "ab"
+let str2 = "cd"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 20) //120
+// console.log(100 + "2")  //1002
+//console.log("10" + 2 + 2) //1022
+//console.log(1 + 2 + "20") //320
+ console.log(100 + "2"+"2")  //1004
---
+++ Fundamentals _of_js/01basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 29469;
+let accounteEmail = "ksau@gmail.com"
+var accountPassword = "28456"
+  accountCity = "Jaipur"
+
+
+  accountCity =" patna"
+   //let accountEmail = "@gmail"
+  var accountPassword = "93645"
+  //accountId = 11111;
+  
+
+
+console.log(accountId);
+console.table([accountId,accountPassword, accountCity, accounteEmail,])
